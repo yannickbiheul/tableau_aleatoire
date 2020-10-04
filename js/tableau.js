d689cc0b9e478a8bb7d1d1fc255eb74ecddf60f7
Yannick Biheul <yannickbiheul@outlook.fr>
2020-10-04 19:11:01 +0200

    Essai réussi avec 10 questions

--- js/tableau.js
+++ js/tableau.js
@@ -11,7 +11,12 @@ let question2 = "Question 2";
 let question3 = "Question 3";
 let question4 = "Question 4";
 let question5 = "Question 5";
-let questions = [question1, question2, question3, question4, question5];
+let question6 = "Question 6";
+let question7 = "Question 7";
+let question8 = "Question 8";
+let question9 = "Question 9";
+let question10 = "Question 10";
+let questions = [question1, question2, question3, question4, question5, question6, question7, question8, question9, question10];
 
 // Tableau random
 
