bfc55761bbe27b6b4ea7d58771118048cd03a317
Yannick Biheul <yannickbiheul@outlook.fr>
2020-10-04 18:56:56 +0200

    Essai réussi !!!

--- js/tableau.js
+++ js/tableau.js
@@ -1,5 +1,10 @@
 
-let blocsQuestions = document.querySelectorAll(".blocQuestion");
+let blocQuestion1 = "Bloc Question 1";
+let blocQuestion2 = "Bloc Question 2";
+let blocQuestion3 = "Bloc Question 3";
+let blocQuestion4 = "Bloc Question 4";
+let blocQuestion5 = "Bloc Question 5";
+let blocsQuestions = [blocQuestion1, blocQuestion2, blocQuestion3, blocQuestion4, blocQuestion5];
 
 let question1 = "Question 1";
 let question2 = "Question 2";
@@ -15,14 +20,13 @@ let randoms = [];
 // Fonction tableau random
 
 function randomTab() {
-    for (let i = 0; i <= blocsQuestions.length; i++) {
+    for (let i = 0; i < blocsQuestions.length; i++) {
         let random = Math.floor(Math.random() * questions.length);
-        if (randoms.includes(random)) {
+        while (randoms.includes(random)) {
             random = Math.floor(Math.random() * questions.length);
-        } else {
-            randoms.push(random);
-            blocsQuestions[i].innerText = questions[randoms[i]];
-        }
+        } 
+        randoms.push(random);
+        blocsQuestions[i] = questions[randoms[i]];
     }
     return randoms;
 }
@@ -30,3 +34,4 @@ function randomTab() {
 randomTab();
 
 console.log(randoms);
+console.log(blocsQuestions);
